dcaf4f098934a24ff4af760ff74a444c343be63a
hino-shoma <hino.syo4691@gmail.com>
2024-07-03 19:25:44 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,3 +1,2 @@
 const today = new Date();
-today.setMonth(today.getMonth()+1);
-console.log(today.getFullYear()+"年"+today.getMonth()+"月"+today.getDate()+"日");
+console.log(today.getFullYear()+"年"+(today.getMonth()+1)+"月"+today.getDate()+"日");
